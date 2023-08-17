80b979909d95d7381a586f0e8bdda7c8eab624ce
Vincent Kioko <vincentkioko51@gmail.com>
2023-08-17 11:57:40 +0300

    ApolloClient Init

--- src/index.js
+++ src/index.js
@@ -1,13 +1,44 @@
+import 'cross-fetch/polyfill'
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client/core';
 import 'dotenv/config';
 
-const userCredentials = { firstname: 'Robin' };
-const userDetails = { nationality: 'German' };
+const GET_REPOSITORIES_OF_ORGANIZATION = gql`
+	query($organization: String!){
+		organization(login: $organization){
+			name
+			url
+			repositories(first: 5){
+				edges {
+					node {
+						id
+						name
+						url
+						stargazers {
+							totalCount
+						}
+					}
+				}
+				totalCount
+				pageInfo {
+					endCursor
+					hasNextPage
+				}
+			}
+		}
+	}
+`;
 
-const user = {
-  ...userCredentials,
-  ...userDetails,
-};
+const client = new ApolloClient({
+	uri: 'https://api.github.com/graphql',
+	cache: new InMemoryCache(),
+	headers: {
+		authorization: `Bearer ${process.env.GITHUB_PERSONAL_ACCESS_TOKEN}`,
+	}
+})
 
-console.log(user);
-
-console.log(process.env.SOME_ENV_VARIABLE);
+client.query({
+	query: GET_REPOSITORIES_OF_ORGANIZATION,
+	variables: {
+		organization: "the-road-to-learn-react",
+	}
+}).then(console.log)
